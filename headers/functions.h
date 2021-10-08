c1d8ad52f0b7bf2c255e842012650d38fed9f014
Johnny Robeson <johnny@localmomentum.net>
2021-10-07 22:32:42 -0400

    Reorganize Function members/methods order to match usage
    It goes methods, operators, and members

--- headers/functions.h
+++ headers/functions.h
@@ -6,19 +6,22 @@ class Function {
   public:
     Function() = default;
     Function(int id, std::string n, int declr_pos, int addr, int end);
+
     void add_instruction(const std::shared_ptr<Instruction>& instr);
-    std::vector<std::shared_ptr<Instruction>> instructions;
+    void set_addr(int addr);
+    int get_length_in_bytes();
+
+    friend bool operator<(const Function& f1, const Function& f2);
+    friend bool operator==(const Function& f1, const Function& f2);
+
+    int id = 0;
     std::string name;
     int declr_position = 0; // covers the list of offsets at the beginning
     int actual_addr = 0;    // covers the pointers before that
     int end_addr = 0;
     int xlsx_row_index = 0;
-    int id = 0;
-    friend bool operator<(const Function& f1, const Function& f2);
-    friend bool operator==(const Function& f1, const Function& f2);
     int nb_pad = 0;
-    void set_addr(int addr);
-    int get_length_in_bytes();
+    std::vector<std::shared_ptr<Instruction>> instructions;
 };
 
 std::vector<Function>::iterator find_function_by_name(std::vector<Function>& v, std::string name);
--- sources/functions.cpp
+++ sources/functions.cpp
@@ -3,22 +3,23 @@
 #include <string>
 
 Function::Function(int id, std::string n, int declr_pos, int addr, int end)
-  : name(std::move(n))
+  : id(id)
+  , name(std::move(n))
   , declr_position(declr_pos)
   , actual_addr(addr)
-  , end_addr(end)
-  , id(id) {
+  , end_addr(end) {
     int multiple = 4;
     if (isMultiple0x10(this->name)) multiple = 0x10;
     int nb_byte_to_add = (((int)std::ceil((float)addr / (float)multiple))) * multiple - addr;
     nb_pad = nb_byte_to_add;
 }
+
 void Function::add_instruction(const std::shared_ptr<Instruction>& instr) {
     if (instr->get_OP() > 0) instructions.push_back(instr);
 }
 
-bool operator<(const Function& f1, const Function& f2) { return f1.actual_addr < f2.actual_addr; }
 void Function::set_addr(int addr) { this->actual_addr = addr; }
+
 int Function::get_length_in_bytes() {
 
     int length_in_bytes = 0;
@@ -27,10 +28,15 @@ int Function::get_length_in_bytes() {
     }
     return length_in_bytes;
 }
+
+bool operator<(const Function& f1, const Function& f2) { return f1.actual_addr < f2.actual_addr; }
+
 bool operator==(const Function& f1, const Function& f2) { return f1.id == f2.id; }
+
 std::vector<Function>::iterator find_function_by_name(std::vector<Function>& v, std::string name) {
     return find_if(v.begin(), v.end(), [&name](const Function& obj) { return obj.name == name; });
 }
+
 std::vector<Function>::iterator find_function_by_id(std::vector<Function>& v, int id) {
     return find_if(v.begin(), v.end(), [&id](const Function& obj) { return obj.id == id; });
 }
